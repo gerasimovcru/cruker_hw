e4d95b84df334082c59b4501c316c496bbc698b8
gerasimovcru <92694651+gerasimovcru@users.noreply.github.com>
2021-10-20 13:17:26 +0300

    mi

--- 3/poisk.js
+++ 3/poisk.js
@@ -1,54 +1,78 @@
-str = "";//любая строка
-
-max_vord(str);
+poisk.onclick = function() {
+	let str = document.getElementById('text').value;	
+	max_vord(str);
+}
 
  function max_vord(str){
-
-    if(str.length > 3){
-    let split_str = str.split(/[\s* , . ? ! ; : + - _ = ( ) " ' ` @ # $ % ^ & * № < > − 1 2 3 4 5 6 7 8 9 0]+/);//добавить любые символы которые не могут быть "словами" 
-
-    let vord = [];
+	 
+	let vord = [];
     let i = 0;
 
-    let col = new Map();
-    col.set(split_str[0], 1);
-    let a;
-    for(let i = 1; i<split_str.length; i++) {
-        if(col.has(split_str[i])){
-            a = col.get(split_str[i]);
-            col.set(split_str[i], a+1);
-        } else{
-            col.set(split_str[i], 1);
-        }        
-    }
+	let vords = split_str_f(str);
    
-    i = 0;
-    for (let value of col.entries()) {
-        vord[i] = value;
-        i++;   
-      }
-
-      vord.sort((a, b) => (b[1] - a[1]));
-  
-
-      if(vord.length>=3){
-      console.log(vord[0][0]);
-      console.log(vord[1][0]);
-      console.log(vord[2][0]);
-      }else{
-         if(vord.length == 2){
-            console.log(vord[0][0]);
-            console.log(vord[1][0]);  
-         }else{
-             console.log(vord[0][0]);
-         }
-      }
-
-    }else{
-        console.log("ERR");
-    }
+	if(vords.size > 0){
+	
+		i = 0;
+		for (let value of vords.entries()) {
+			vord[i] = value;
+			console.log(value);
+			i++;   
+		}
+	
+		vord.sort((a, b) => (b[1] - a[1]));
+	
+	
+		if(vord.length>=3){
+			console.log(vord[0][0]);
+			console.log(vord[1][0]);
+			console.log(vord[2][0]);
+			alert(`Vords: ${vord[0][0]}, ${vord[1][0]}, ${vord[2][0]}`);
+			}else{
+				if(vord.length == 2){
+					console.log(vord[0][0]);
+					console.log(vord[1][0]);
+					alert(`Vords: ${vord[0][0]}, ${vord[1][0]}`);					
+				}else{
+					console.log(vord[0][0]);
+					alert(`Vords: ${vord[0][0]}`);
+				}
+			}
+		vords.clear();
+	}else{
+		console.log("ERROR: Line is empty or line contains only empty characters");
+		alert("ERROR: Line is empty or line contains only empty characters");
+		vords.clear();
+	}
 
  }
 
- 
-
+ function split_str_f(str){
+	 
+	let vords = new Map();
+	 
+//	if(str.length > 0){
+		let split_str = str.split(/[\s* , . ? ! ; : + - _ = ( ) " ' ` @ # $ % ^ & * № < > − 1 2 3 4 5 6 7 8 9 0]+/);//добавить любые символы которые не могут быть "словами" 
+			
+		//let vords = new Map();
+		vords.set(split_str[0], 1);
+		let a;
+		for(let i = 1; i<split_str.length; i++) {
+			if(vords.has(split_str[i])){
+				a = vords.get(split_str[i]);
+				vords.set(split_str[i], a+1);
+			} else{
+				vords.set(split_str[i], 1);
+			}        
+		}
+		
+		vords.delete('');
+		
+		return vords;
+//	}else{
+//		console.log("ERROR: Line is empty");
+//		alert("ERROR: Line is empty");
+//		vords.clear();
+//		return vords;					
+//    }
+	 
+ }
