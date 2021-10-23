4db9f6dbcb109e0625a0ad9e0d65fc5182a60787
gerasimovcru <92694651+gerasimovcru@users.noreply.github.com>
2021-10-23 14:14:41 +0300

    mi

--- 3/poisk.js
+++ 3/poisk.js
@@ -14,8 +14,9 @@ poisk.onclick = function() {
 	
 		i = 0;
 		for (let value of vords.entries()) {
-			vord[i] = value;
-			console.log(value);
+            vord[i] = value;
+
+
 			i++;   
 		}
 	
@@ -33,8 +34,8 @@ poisk.onclick = function() {
 					console.log(vord[1][0]);
 					alert(`Vords: ${vord[0][0]}, ${vord[1][0]}`);					
 				}else{
-					console.log(vord[0][0]);
-					alert(`Vords: ${vord[0][0]}`);
+                        console.log(vord[0][0]);
+                        alert(`Vords: ${vord[0][0]}`);
 				}
 			}
 		vords.clear();
@@ -50,29 +51,45 @@ poisk.onclick = function() {
 	 
 	let vords = new Map();
 	 
-//	if(str.length > 0){
-		let split_str = str.split(/[\s* , . ? ! ; : + - _ = ( ) " ' ` @ # $ % ^ & * № < > − 1 2 3 4 5 6 7 8 9 0]+/);//добавить любые символы которые не могут быть "словами" 
-			
-		//let vords = new Map();
-		vords.set(split_str[0], 1);
-		let a;
-		for(let i = 1; i<split_str.length; i++) {
-			if(vords.has(split_str[i])){
-				a = vords.get(split_str[i]);
-				vords.set(split_str[i], a+1);
-			} else{
-				vords.set(split_str[i], 1);
-			}        
-		}
+        
+		//let split_str = str.split(/[\s* , . ? ! ; : + - _ = ( ) " ' ` @ # $ % ^ & * № < > − 1 2 3 4 5 6 7 8 9 0]+/);//добавить любые символы которые не могут быть "словами" 
 		
-		vords.delete('');
+        let split_str = str.split(/[\s* , . ? ! ; : " ' ` - _ ( ) − { } [ = + < >]+/);
+        
+        split_str = delete_not_words(split_str);
+
+        //console.log(split_str.length);
+       
+        if(split_str.length>0){
+		    vords.set(split_str[0], 1);
+		    let a;
+		    for(let i = 1; i<split_str.length; i++) {
+
+                if(vords.has(split_str[i])){
+				    a = vords.get(split_str[i]);
+				    vords.set(split_str[i], a+1);
+			    } else{
+				    vords.set(split_str[i], 1);
+			    }        
+		    }
+		
+		    vords.delete('');
+            vords.delete('-');//
+            vords.delete('−');//я хз они не убираются
+        
+        }
 		
 		return vords;
-//	}else{
-//		console.log("ERROR: Line is empty");
-//		alert("ERROR: Line is empty");
-//		vords.clear();
-//		return vords;					
-//    }
-	 
+       
+ }
+
+ function delete_not_words(split_str){
+
+    for(let i = 0; i<split_str.length; i++){
+        
+        if(split_str[i].split(/[ @ # $ % ^ & * № 1 2 3 4 5 6 7 8 9 0]+/).length > 1){
+            split_str.splice(i, 1);           
+        }
+    }
+    return split_str;
  }
